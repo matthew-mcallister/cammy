730a3cfe7d8b119e2dc6f54ddd3fa549c5aa5350
Matthew McAllister <matthew.mcallister.0@gmail.com>
2025-12-29 16:26:02 -0700

    Improved single-threaded with BFS
    Not as fast as multithreaded but better than before

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,22 @@
 
 #![feature(iter_macro, yield_expr)]
 
+use std::collections::VecDeque;
+use std::collections::hash_map::Entry;
 use std::iter::iter;
 
 use fnv::FnvHashMap;
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-struct State<const D: usize> {
+struct StateKey<const D: usize> {
     x: u16,
-    held: u16,
     piles: [u16; D],
+}
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+struct State<const D: usize> {
+    inner: StateKey<D>,
+    held: u16,
 }
 
 impl<const D: usize> State<D> {
@@ -22,16 +29,22 @@ impl<const D: usize> State<D> {
         let state = self;
         let it = iter!(move || {
             if state.held == 0 { return; }
-            if state.x + 1 < D as u16 {
+            if state.inner.x + 1 < D as u16 {
                 State {
-                    x: state.x + 1,
+                    inner: StateKey {
+                        x: state.inner.x + 1,
+                        piles: state.inner.piles,
+                    },
                     held: state.held - 1,
                     ..*state
                 }.yield
             }
-            if state.x > 0 {
+            if state.inner.x > 0 {
                 State {
-                    x: state.x - 1,
+                    inner: StateKey {
+                        x: state.inner.x - 1,
+                        piles: state.inner.piles,
+                    },
                     held: state.held - 1,
                     ..*state
                 }.yield
@@ -42,10 +55,10 @@ impl<const D: usize> State<D> {
 
     fn pickup<const C: u16>(&mut self) {
         let state = self;
-        let pile = state.x as usize;
-        let pickup = std::cmp::min(C - state.held, state.piles[pile]);
+        let pile = state.inner.x as usize;
+        let pickup = std::cmp::min(C - state.held, state.inner.piles[pile]);
         state.held += pickup;
-        state.piles[pile] -= pickup;
+        state.inner.piles[pile] -= pickup;
     }
 
     fn successors<const C: u16>(&self) -> impl Iterator<Item = State<D>> + '_ {
@@ -56,7 +69,7 @@ impl<const D: usize> State<D> {
                 for amount in 0..=state.held {
                     let mut state = state;
                     state.held -= amount;
-                    state.piles[state.x as usize] += amount;
+                    state.inner.piles[state.inner.x as usize] += amount;
                     state.yield;
                 }
             }
@@ -66,69 +79,85 @@ impl<const D: usize> State<D> {
 }
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-struct Node<const D: usize> {
-    prev: Option<State<D>>,
-    visited: bool,
+struct StateMeta<const D: usize> {
+    prev: Option<StateKey<D>>,
+    held: u16,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Solver<const D: usize> {
-    states: FnvHashMap<State<D>, Node<D>>,
+    states: FnvHashMap<StateKey<D>, StateMeta<D>>,
+    bananas_sold: u16,
+    solutions: Vec<State<D>>,
 }
 
 impl<const D: usize> Solver<D> {
-    /// Returns an iterator over states that attain maximal distance from
-    /// start, with maximal number of bananas held at that position.
-    fn solutions(&self) -> impl Iterator<Item = (&'_ State<D>, &'_ Node<D>)> + '_ {
-        let max_x = self.states.keys().map(|s| s.x).max().unwrap_or(0);
-        let max_held = self.states.keys()
-            .filter(|s| s.x == max_x)
-            .map(|s| s.held)
-            .max()
-            .unwrap_or(0);
-        self.states.iter()
-            .filter(move |(s, _)| s.x == max_x && s.held == max_held)
+    fn insert(&mut self, prev: Option<StateKey<D>>, state: State<D>) -> bool {
+        let meta = StateMeta {
+            prev,
+            held: state.held,
+        };
+        match self.states.entry(state.inner) {
+            Entry::Occupied(e) => {
+                // We never overwrite an entry because we visit in order of
+                // depth from the root
+                assert!(e.get().held >= meta.held);
+                false
+            }
+            Entry::Vacant(e) => {
+                e.insert(meta);
+                if state.inner.x == (D - 1) as u16 {
+                    if state.held > self.bananas_sold {
+                        self.bananas_sold = state.held;
+                        self.solutions = vec![state];
+                    } else if state.held == self.bananas_sold {
+                        self.solutions.push(state);
+                    }
+                }
+                true
+            }
+        }
     }
 }
 
 fn solve<const D: usize, const C: u16>(bananas: u16) -> Solver<D> {
     let start = std::time::Instant::now();
 
-    let mut solver = Solver {
-        states: Default::default(),
-    };
+    let mut solver = Solver::default();
+    let held = std::cmp::min(bananas, C);
     let mut initial = State {
-        x: 0,
-        held: 0,
-        piles: [0; D],
+        inner: StateKey {
+            x: 0,
+            piles: [0; D],
+        },
+        held,
     };
-    initial.piles[0] = bananas;
-    initial.pickup::<C>();
-    solver.states.insert(initial, Node { prev: None, visited: false });
+    initial.inner.piles[0] = bananas - held;
+    solver.insert(None, initial);
 
-    let mut work = vec![initial];
-    while let Some(state) = work.pop() {
-        let node = solver.states.get_mut(&state).unwrap();
-        if node.visited {
-            continue;
-        }
-        node.visited = true;
-        
+    let mut work = VecDeque::new();
+    work.push_back(initial);
+
+    while let Some(state) = work.pop_front() {
         for succ in state.successors::<C>() {
-            solver.states.entry(succ).or_insert(Node { prev: Some(state), visited: false });
-            work.push(succ);
+            if solver.insert(Some(state.inner), succ) {
+                work.push_back(succ);
+            }
         }
     }
 
     let elapsed = start.elapsed();
 
-    println!("done solving. visited {} states in {:.2}s", solver.states.len(), elapsed.as_secs_f64());
+    println!("visited {} states in {:.2}s", solver.states.len(), elapsed.as_secs_f64());
 
     solver
 }
 
 fn main() {
-    let solver = solve::<15, 5>(120);
-    let (state, _node) = solver.solutions().next().unwrap();
-    println!("{:?}", state);
+    let solver = solve::<15, 5>(100);
+    if let Some(s) = solver.solutions.first() {
+        println!("{:?}", s);
+    } else {
+        println!("no solutions.");
+    }
 }
